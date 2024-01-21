f509036871f85d3144489ccb4759595f87bf4f54
Nicolás Bertoni <nicobertoni99.akd@gmail.com>
2024-01-21 15:16:13 -0300

    Update Categoria.java

--- src/main/java/com/platzi/market/persistence/entities/Categoria.java
+++ src/main/java/com/platzi/market/persistence/entities/Categoria.java
@@ -11,4 +11,28 @@ public class Categoria {
     private Integer idCategoria;
     private String descripcion;
     private Integer estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
 }
